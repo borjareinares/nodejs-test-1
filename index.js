26424ff052c6d26bf70ef2ca765c1b981508b5a3
borjareinares <borjareinares@gmail.com>
2019-10-25 20:02:26 +0200

    ENV variable

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
 
+const pod_ip = process.env.POD_IP;
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World! ${pod_ip}')
 })
 
 app.listen(8081, function () {
